9ce49cc40e601bd266d313f14607f1e8f6eee4a5
persianpros <persianpros@gmail.com>
2020-11-04 16:26:39 +0330

    import

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-settings [![Build Status](https://travis-ci.org/OpenVisionE2/openvision-xml.svg?branch=master)](https://travis-ci.org/OpenVisionE2/openvision-xml) [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
-=========
+settings [![Build Status](https://travis-ci.org/persianpros/test.svg?branch=master)](https://travis-ci.org/persianpros/test) [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+========
 settings
---
+++ build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+rm -rf GioppyGio*
+rm -rf *index*
+
+lynx -dump http://picons.gioppygio.it/index.php?dir=settings/DREAM%20E2%2BDTT/Italia/ | awk '/zip/{print $2}' > links.txt
+
+find -type f -name "links.txt" | xargs -L1 sed -i '/http/d'
+
+find -type f -name "links.txt" | xargs -L1 sed -i 's|GioppyGio_|http://picons.gioppygio.it/index.php?dir=settings/DREAM%20E2%2BDTT/Italia/\&file\=GioppyGio_|g'
+
+find -type f -name "links.txt" | xargs -L1 sed -i 's|+|%2B|g'
+
+wget --wait=3 -i links.txt
+
+rm -f links.txt
+
+for i in *.zip
+do
+    mv "$i" "`echo $i | sed 's/index.php?dir=settings%2FDREAM E2+DTT%2FItalia%2F&file=//'`"
+done
+
+for d in *.zip
+do
+  dir=./${d%%.zip}
+  unzip -d "$dir" "$d"
+done
+
+rm -rf *.zip
+
+rename 's/_[^_]*$//' */
+
+setup_git() {
+  git config --global user.email "bot@openvision.tech"
+  git config --global user.name "Open Vision bot"
+}
+
+commit_files() {
+  git checkout master
+  git add -u
+  git add *
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add upstream https://${GH_TOKEN}@github.com/persianpros/test.git > /dev/null 2>&1
+  git push --quiet upstream master || echo "failed to push with error $?"
+}
+
+setup_git
+commit_files
+upload_files
