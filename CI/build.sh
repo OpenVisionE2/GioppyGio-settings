1c76517649d7e259f216bdb91cca4a1ae8e9478d
Persian Prince <persianpros@yahoo.com>
2022-05-24 02:51:00 +0430

    Update build.sh

--- CI/build.sh
+++ CI/build.sh
@@ -3,25 +3,19 @@
 # Script by Persian Prince for https://github.com/OpenVisionE2
 # You're not allowed to remove my copyright or reuse this script without putting this header.
 
+cd ..
 rm -rf GioppyGio*
 rm -rf *index*
+cd CI
+rm -rf *.zip
 
-lynx -dump http://picons.gioppygio.it/index.php?dir=settings/DREAM%20E2/ | awk '/zip/{print $2}' > links.txt
+lynx -dump https://gioppygio.it/settings/e2/ | awk '/zip/{print $2}' > links.txt
 
-find -type f -name "links.txt" | xargs -L1 sed -i '/http/d'
-
-find -type f -name "links.txt" | xargs -L1 sed -i 's|GioppyGio_|http://picons.gioppygio.it/index.php?dir=settings/DREAM%20E2/\&file\=GioppyGio_|g'
-
-find -type f -name "links.txt" | xargs -L1 sed -i 's|+|%2B|g'
+find -type f -name "links.txt" | xargs -L1 sed -i '/]/d'
 
 wget --wait=3 -i links.txt
 
 rm -f links.txt
-
-for i in *.zip
-do
-    mv "$i" "`echo $i | sed 's/index.php?dir=settings%2FDREAM E2%2F&file=//'`"
-done
 
 for d in *.zip
 do
@@ -31,37 +25,12 @@ done
 
 rm -rf *.zip
 
-rename 's/_[^_]*$//' */
-
-lynx -dump http://picons.gioppygio.it/index.php?dir=settings/DREAM%20E2%2BDTT/Italia/ | awk '/zip/{print $2}' > links.txt
-
-find -type f -name "links.txt" | xargs -L1 sed -i '/http/d'
-
-find -type f -name "links.txt" | xargs -L1 sed -i 's|GioppyGio_|http://picons.gioppygio.it/index.php?dir=settings/DREAM%20E2%2BDTT/Italia/\&file\=GioppyGio_|g'
-
-find -type f -name "links.txt" | xargs -L1 sed -i 's|+|%2B|g'
-
-wget --wait=3 -i links.txt
-
-rm -f links.txt
-
-for i in *.zip
-do
-    mv "$i" "`echo $i | sed 's/index.php?dir=settings%2FDREAM E2+DTT%2FItalia%2F&file=//'`"
-done
-
-for d in *.zip
-do
-  dir=./${d%%.zip}
-  unzip -d "$dir" "$d"
-done
-
-rm -rf *.zip
-
-rename 's/_[^_]*$//' */
+rename -f 's/_[^_]*$//' */
 
 find . -name '*satellites.xml*' -type f | xargs rm -f
 find . -name '*.url*' -type f | xargs rm -f
+
+mv -f GioppyGio* ../
 
 setup_git() {
   git config --global user.email "bot@openvision.tech"
